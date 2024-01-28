827bc9ebcea5939986bb491d2b4a32e4e6425d3c
UdayBari3421 <udaybari2003@gmail.com>
2024-01-28 19:02:30 +0530

    submitting code-Sun Jan 28 2024 19:02:28 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
+  let [value, setValue] = useState("");
+  let [items, setItems] = useState(fruits);
+
+  useEffect(() => {
+    let filterdata = items.filter((item) => {
+      return item.includes(value);
+    });
+    value ? setItems(filterdata) : setItems(fruits);
+  }, [value]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <div>
+        <ul>
+          {items &&
+            items.map((item, index) => {
+              return <li key={item + index}>{item}</li>;
+            })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
